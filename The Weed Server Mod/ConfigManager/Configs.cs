9ff351e15bcb86be112434abd89a25a427f47219
endergriffen <endergriffen@gmail.com>
2025-03-28 22:40:16 -0400

    Add project files.

---
+++ The Weed Server Mod/ConfigManager/Configs.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx.Configuration;
+using System;
+
+namespace The_Weed_Server_Mod.ConfigManager
+{
+    public class Configs
+    {
+        private static Configs instance;
+
+        public static Configs Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Configs();
+                }
+                return instance;
+            }
+        }
+
+        public ConfigEntry<float> ExampleFloatConfig { get; private set; }
+        public ConfigEntry<int> ExampleIntConfig { get; private set; }
+        public ConfigEntry<string> ExampleStringConfig { get; private set; }
+        public ConfigEntry<bool> ExampleBoolConfig { get; private set; }
+
+        public void Setup(ConfigFile config)
+        {
+            ExampleFloatConfig = config.Bind("Float", "Example Float Config", 0f, "This is simply a example of any configs that require a float.");
+            ExampleIntConfig = config.Bind("Int", "Example Int Config", 0, "This is simply a example of any configs that require a int.");
+            ExampleStringConfig = config.Bind("String", "Example String Config", "apple", "This is simply a example of any configs that require a string.");
+            ExampleBoolConfig = config.Bind("Bool", "Example Bool Config", false, "This is simply a example of any configs that require a bool.");
+        }
+    }
+}
---
+++ The Weed Server Mod/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+using The_Weed_Server_Mod.ConfigManager;
+
+namespace The_Weed_Server_Mod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "Weed_And_Hope.The_Weed_Server_Mod";
+        private const string modName = "The_Weed_Server_Mod";
+        private const string modVersion = "1.0.0";
+
+        public readonly Harmony harmony = new Harmony(modGUID);
+        public static Plugin Instance;
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource("TWSM");
+            mls.LogInfo("The Weed Server Mod for REPO is alive!");
+
+            Configs.Instance.Setup(Config);
+
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(Configs));
+        }
+    }
+}
