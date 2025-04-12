3f54e0f73a4cfdf2392da91c510b390433915ed2
endergriffen <endergriffen@gmail.com>
2025-04-11 22:27:12 -0400

    apple

--- The Weed Server Mod/TruckScreen/Display Level Command.cs
+++ The Weed Server Mod/TruckScreen/Display Level Command.cs
@@ -13,12 +13,10 @@ namespace The_Weed_Server_Mod.TruckScreen
                 PhotonView pv = truckScreen.GetComponent<PhotonView>();
                 if (pv != null)
                 {
-                    // Format the decideLevel so that it is distinct on the truck screen.
                     string formattedLevel = "<color=#00FF00>VOTE ON THE NEXT LEVEL WITH /vote {level}:</color>\n\n" +
                         "<color=#D4A276>MANOR:</color>\n <size=0.25><mark=#6f4518>lll </mark></size>\n\n" +
                         "<color=#7241b4>WIZARD:</color>\n <size=0.25><mark=#502d7e>lll </mark></size>\n\n" +
                         "<color=#7CA1C2>ARTIC:</color>\n <size=0.25><mark=#3C6080>lll </mark></size>";
-                    // Use the vanilla RPC implemented in TruckScreenText.
                     pv.RPC("MessageSendCustomRPC", RpcTarget.All, "", formattedLevel);
                 }
                 else
